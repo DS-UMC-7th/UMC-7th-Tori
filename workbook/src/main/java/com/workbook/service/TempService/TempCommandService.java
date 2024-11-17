764f02e5739ea205dd0523bb706706df6ce71264
alschlee <1daysixmeal@gmail.com>
2024-11-17 19:40:33 +0900

    [FEAT] 7주차 워크북 미션 #5

---
+++ workbook/src/main/java/com/workbook/service/TempService/TempCommandService.java
@@ -0,0 +1,5 @@
+package com.workbook.service.TempService;
+
+public interface TempCommandService {
+    void CheckFlag(Integer flag);
+}
---
+++ workbook/src/main/java/com/workbook/service/TempService/TempQueryService.java
@@ -0,0 +1,6 @@
+package com.workbook.service.TempService;
+
+public interface TempQueryService {
+
+    void CheckFlag(Integer flag);
+}
